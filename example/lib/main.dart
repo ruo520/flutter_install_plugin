d9d492a8a59d06c1c30a7033683d83bd8990feef
suiyigou <370484600@qq.com>
2022-04-12 10:46:18 +0800

    1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-12 10:18:23
+ * @LastEditTime: 2022-04-12 10:45:54
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /flutter_install_plugin/example/lib/main.dart
+ */
 import 'package:flutter/material.dart';
 import 'package:install_plugin/install_plugin.dart';
 import 'package:permission_handler/permission_handler.dart';
