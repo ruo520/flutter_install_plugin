f302a2bfd0fe64c91866c8f9074a5f2aba85abff
suiyigou <370484600@qq.com>
2022-04-20 17:32:24 +0800

    1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,8 +3,8 @@
 /*
  * @Author: your name
  * @Date: 2022-04-12 10:18:23
- * @LastEditTime: 2022-04-12 10:45:54
- * @LastEditors: your name
+ * @LastEditTime: 2022-04-20 17:20:52
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /flutter_install_plugin/example/lib/main.dart
  */
@@ -62,9 +62,10 @@ class _MyAppState extends State<MyApp> {
       print('make sure the apk file is set');
       return;
     }
-    Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-    if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
+    // Map<PermissionGroup, PermissionStatus> permissions =
+    //     await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    var status = await Permission.storage.status;
+    if (status == PermissionStatus.granted) {
       InstallPlugin.installApk(_apkFilePath, 'com.zaihui.installpluginexample')
           .then((result) {
         print('install apk $result');
